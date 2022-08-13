45e5b72dc735e7c41835359015affd414b67151d
Your Name <nhat1234559@gmail.com>
2022-08-13 22:53:08 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,21 @@
+const play = document.getElementById('playAudio');
+const sound = document.getElementById('sound');
+var check = 0;
+
+function playAudio(){
+    play.addEventListener("click",function(){
+       if(check == 0) {
+            check = 1;
+            sound.play();
+            play.innerHTML = `<i class="fa-solid fa-pause"></i>` ;
+       }else{
+            check = 0;
+            sound.pause();
+            play.innerHTML = `<i class="fa-solid fa-play">` ;
+       }    
+    });
+    
+
+}
+
+playAudio();
